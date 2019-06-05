864b6371921fe550f65548dfc26bb7529010cec5
sqoshi <piotrpopis98@icloud.com>
2019-06-06 00:47:20 +0200

    up

--- List4/src/Z2Receiver.java
+++ List4/src/Z2Receiver.java
@@ -2,57 +2,69 @@ import java.net.*;
 
 public class Z2Receiver
 {
-static final int datagramSize=50;
-InetAddress localHost;
-int destinationPort;
-DatagramSocket socket;
+    static final int datagramSize=50;
+    InetAddress localHost;
+    int destinationPort;
+    DatagramSocket socket;
 
-ReceiverThread receiver;
+    ReceiverThread receiver;
 
-public Z2Receiver(int myPort, int destPort)
-throws Exception
+    DatagramQueue datagramQueue;
+
+    public Z2Receiver(int myPort, int destPort)
+            throws Exception
     {
-    localHost=InetAddress.getByName("127.0.0.1");
-    destinationPort=destPort;
-    socket=new DatagramSocket(myPort);
-    receiver=new ReceiverThread();    
+        localHost=InetAddress.getByName("127.0.0.1");
+        destinationPort=destPort;
+        socket=new DatagramSocket(myPort);
+        receiver=new ReceiverThread();
+        datagramQueue = new DatagramQueue();
     }
 
-class ReceiverThread extends Thread
-{
-
-public void run()
+    class ReceiverThread extends Thread
     {
-    try
-	{
-	    while(true)
-		{
-		    byte[] data=new byte[datagramSize];
-		    DatagramPacket packet= 
-			new DatagramPacket(data, datagramSize);
-		    socket.receive(packet);
+
+        public void run()
+        {
+            try
+            {
+                while(true)
+                {
+                    byte[] data=new byte[datagramSize];
+                    DatagramPacket packet=
+                            new DatagramPacket(data, datagramSize);
+                    socket.receive(packet);
                     Z2Packet p=new Z2Packet(packet.getData());
-		    System.out.println("R:"+p.getIntAt(0)
-				       +": "+(char) p.data[4]);
+                    System.err.println("Received:"+p.getIntAt(0)
+                            +": "+(char) p.data[4]);
+
+                    datagramQueue.addPacket(p);
+                    while ((p = datagramQueue.getCorrectPacket()) != null){
+                        System.out.println("R:"+p.getIntAt(0)
+                                +": "+(char) p.data[4]);
+                    }
+
                     // WYSLANIE POTWIERDZENIA
                     packet.setPort(destinationPort);
                     socket.send(packet);
-		}
-	}
-    catch(Exception e)
-	{
-        System.out.println("Z2Receiver.ReceiverThread.run: "+e);
-	}
+                }
+            }
+            catch(Exception e)
+            {
+                System.out.println("Z2Receiver.ReceiverThread.run: "+e);
+                e.printStackTrace();
+            }
+        }
+
+
     }
 
-}
-
-public static void main(String[] args)
-throws Exception
+    public static void main(String[] args)
+            throws Exception
     {
-	Z2Receiver receiver=new Z2Receiver( Integer.parseInt(args[0]),
-				   Integer.parseInt(args[1]));
-	receiver.receiver.start();
+        Z2Receiver receiver=new Z2Receiver( Integer.parseInt(args[0]),
+                Integer.parseInt(args[1]));
+        receiver.receiver.start();
     }
 
 
